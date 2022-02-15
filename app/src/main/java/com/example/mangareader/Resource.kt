e179a42b77601e389e4de141d7441daf86596dd9
stepanowich <ivanov.pavel.stepanowicht@gmail.com>
2022-02-15 19:53:26 +0300

    Fixed the bag with start app.

--- app/src/main/java/com/example/mangareader/Resource.kt
+++ app/src/main/java/com/example/mangareader/Resource.kt
@@ -1,8 +1,9 @@
 package com.example.mangareader
+import android.os.AsyncTask
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 
-abstract class Resource {
+abstract class Resource: AsyncTask<String, Void, RSSFeed> {
 
     public fun getInfo(link: String): Document {
         return Jsoup.connect(link).get()
