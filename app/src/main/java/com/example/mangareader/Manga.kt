2c867016cd820e623b2d04fab98d83b8cae0fe6c
Asir0o <Maxgaliev@yahoo.com>
2022-02-18 01:41:39 +0300

    Сделан парсер страницы с мангой который возвращает класс Manga с данными о манге

--- app/src/main/java/com/example/mangareader/Manga.kt
+++ app/src/main/java/com/example/mangareader/Manga.kt
@@ -2,29 +2,30 @@ package com.example.mangareader
 
 class Manga(
     name: String,
-    numberOfVolumes: String,
+    numberOfVolumesAndStatus: String,
     mainTranslator: String,
     statusTranslation: String,
-    statusReleaseManga: String,
     genres: String,
     category: String,
     screenwriters: String,
     painters: String,
     yearOfIssue: String,
     tags: String,
+    titleImage: String,
     mangaChapters: MutableList<MangaChapter>
 ) {
     var name: String = name  //Название манги
-    var numberOfVolumes: String = numberOfVolumes //Количество томов
+    var numberOfVolumesAndStatus: String =
+        numberOfVolumesAndStatus //Количество томов и статус выхода манги
     var mainTranslator: String = mainTranslator //Основной переводчик
     var statusTranslation: String = statusTranslation //Статус перевода манги
-    var statusReleaseManga: String = statusReleaseManga //Статус выхода манги
     var genres: String = genres //Жанры
     var category: String = category //Категория
     var screenwriters: String = screenwriters //Сценаристы
     var painters: String = painters //Художники
     var yearOfIssue: String = yearOfIssue //Год выпуска
     var tags: String = tags //Теги
+    var titleImage: String = titleImage
     var mangaChapters: MutableList<MangaChapter> = mangaChapters
 
     //    class Manga constructor(
--- app/src/main/java/com/example/mangareader/Parser.kt
+++ app/src/main/java/com/example/mangareader/Parser.kt
@@ -1,5 +1,6 @@
 package com.example.mangareader
 
+import android.media.Image
 import android.util.Log
 import org.jsoup.nodes.Element
 import org.jsoup.select.Elements
@@ -7,36 +8,73 @@ import org.jsoup.select.Elements
 
 class Parser : Resource() {
 
-    public fun parsTitle() {
+    public fun parsTitle(): Manga {
         val doc = getInfo("https://readmanga.io/podniatie_urovnia_v_odinochku__A56ff")
-        var name : String = doc.select("span.name").text()
-        var description : String = doc.select("div.manga-description").text()
-        var translate : String = doc.select("div.subject-meta p:eq(3)").text()
-        var transletor : String = doc.select("div.subject-meta p:eq(2)").text()
-        var volume : String = doc.select("div.subject-meta p:eq(1)").attr("#text")
+        val name: String = doc.select("span.name").text()
+        val description: String = doc.select("div.manga-description").text()
+        val statusTranslation: String = doc.select("div.subject-meta p:eq(3)").text()
+        val mainTranslator: String = doc.select("div.subject-meta p:eq(2)").text()
+        val numberOfVolumesAndStatus: String = doc.select("div.subject-meta p:eq(1)").text()
+        var genres: String = ""
+        val titleImage: String = doc.select("div.expandable img:eq(0)").attr("src")
+        val category: String = doc.select("div.subject-meta p:eq(5)").text()
+        var screenwriters: String = ""
+        var painters: String = ""
+        val yearOfIssue = doc.select(".elem_year > .element-link").text()
+        var tags: String = ""
+        val mangaChapters = mutableListOf<MangaChapter>()
+        Log.i("I", titleImage)
+        Log.i("I", yearOfIssue)
 
-        val genres = mutableListOf<String>()
-
-        var i = 0
-        for (element : Element in doc.select("p.elementList > .elem_genre")) {
-            genres.add(element.select(".element-link").text())
-            Log.i("I",genres[i])
-            i++
+        for (element: Element in doc.select("p.elementList > .elem_screenwriter")) {
+            screenwriters += element.select(".person-link").text() + ", "
         }
+        screenwriters = screenwriters.substring(0, screenwriters.length - 2)
+        Log.i("I", screenwriters)
 
-        for (row : Element in doc.select("table.table.table-hover tr")) {
-            MangaChapter(row.select("td.item-title").text(),
-                "https://readmanga.io" + row.select("a").attr("href"))
+        for (element: Element in doc.select("p.elementList > .elem_illustrator")) {
+            painters += element.select(".person-link").text() + ", "
         }
+        painters = painters.substring(0, painters.length - 2)
+        Log.i("I", painters)
 
-//        Manga(name,)
+        for (element: Element in doc.select("p.elementList > .elem_genre")) {
+            genres += element.select(".element-link").text() + ", "
+        }
+        genres = genres.substring(0, genres.length - 2)
+        Log.i("I", genres)
 
+        for (element: Element in doc.select("p.elementList > .elem_tag")) {
+            tags += element.select(".element-link").text() + ", "
+        }
+        tags = tags.substring(0, tags.length - 2)
+        Log.i("I", tags)
+
+        for (row: Element in doc.select("table.table.table-hover tr")) {
+            mangaChapters.add(
+                MangaChapter(
+                    row.select("td.item-title").text(),
+                    "https://readmanga.io" + row.select("a").attr("href")
+                )
+            )
+        }
         Log.i("Name", name)
         Log.i("Description", description)
-        Log.i("I", translate)
-        Log.i("I", volume)
 
-
+        return Manga(
+            name,
+            numberOfVolumesAndStatus,
+            mainTranslator,
+            statusTranslation,
+            genres,
+            category,
+            screenwriters,
+            painters,
+            yearOfIssue,
+            tags,
+            titleImage,
+            mangaChapters
+        )
 
 //        Log.i("Title", doc.title())
 //        val headlines: Elements = doc.select("div.leftContent")
@@ -44,7 +82,6 @@ class Parser : Resource() {
 //            Log.i("Name", element.select("span.name").text())
 //            name = element.select("span.name").text()
 //        }
-
 
 
 //        for (headline in newsHeadlines) {
